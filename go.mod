7213a07e205fa710f1b4ec326da533fb1c4807df
0xpoma <punk@DESKTOP-D6F62GU>
2023-08-27 08:39:55 +0500

    release2/0

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/0xPoma/Rock-Paper-Scissors
+
+go 1.18
+
+require (
+	github.com/go-echarts/go-echarts v1.0.0 // indirect
+	github.com/go-echarts/go-echarts/v2 v2.2.7 // indirect
+	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
+	golang.org/x/sys v0.0.0-20210616045830-e2b7044e8c71 // indirect
+)
--- new.go
+++ new.go
@@ -1,48 +1,89 @@
 package main
 
-import "fmt"
-import "time"
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"time"
 
-func main(){
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+)
+
+func main() {
 	var choice int
+	var countDraw, countWin, countLose int
 	fmt.Println("--Rock,Paper,Scissors--")
 	fmt.Println(">>1.Rock 2.Paper 3.Scissors")
-	fmt.Scan(&choice)
+	// fmt.Scan(&choice)
 	rand.Seed(time.Now().UnixNano())
-	intchoice:=rand.Intn(3)
-	switch choice {
+
+	for i := 0; i < 1000; i++ {
+		choice = rand.Intn(3) + 1
+		intchoice := rand.Intn(3)
+		switch choice {
 		case 1: // Rock
-			if (intchoice == 0){
-				fmt.Println("Draw")
+			if intchoice == 0 {
+				// fmt.Println("Draw")
+				countDraw++
 			}
-			if(intchoice == 1){ //Paper
-				fmt.Println("You lose")
+			if intchoice == 1 { //Paper
+				// fmt.Println("You lose")
+				countLose++
 			}
-			if (intchoice == 2){ 
-				fmt.Println("You win") //Scissors
+			if intchoice == 2 {
+				// fmt.Println("You win") //Scissors
+				countWin++
 			}
 		case 2: //Paper
-			if (intchoice == 0){
-				fmt.Println("You win")
+			if intchoice == 0 {
+				// fmt.Println("You win")
+				countWin++
 			}
-			if(intchoice == 1){
-				fmt.Println("Draw")
+			if intchoice == 1 {
+				// fmt.Println("Draw")
+				countDraw++
 			}
-			if (intchoice == 2){
-				fmt.Println("You lose")
+			if intchoice == 2 {
+				// fmt.Println("You lose")
+				countLose++
 			}
 		case 3: //Scissors
-			if (intchoice == 0) { 
-				fmt.Println("You lose")
+			if intchoice == 0 {
+				// fmt.Println("You lose")
+				countLose++
 			}
-			if (intchoice == 1) {
-				fmt.Println("You win")
+			if intchoice == 1 {
+				// fmt.Println("You win")
+				countWin++
 			}
-			if (intchoice == 2){
-				fmt.Println("Draw")
+			if intchoice == 2 {
+				// fmt.Println("Draw")
+				countDraw++
 			}
 		default:
 			fmt.Println("Something went wrong")
-			}
+		}
+	}
+	bar := charts.NewBar()
+	bar.SetGlobalOptions(charts.WithTitleOpts(opts.Title{
+		Title: "Comparing ",
+	}))
+
+	bar.SetXAxis([]string{"Win", "Lose", "Draw"}).
+		AddSeries("Numbers", getDiagramm(countWin, countLose, countDraw))
+	f, _ := os.Create("bar.html")
+	bar.Render(f)
+	exec.Command("xdg-open", "bar.html").Run()
+	// exec.Command("cmd", "/C", "start", "bar.html").Start()
+}
+
+func getDiagramm(countWin, countLose, countDraw int) []opts.BarData {
+	values := []opts.BarData{
+		{Value: countWin},
+		{Value: countLose},
+		{Value: countDraw},
+	}
+	return values
 }
