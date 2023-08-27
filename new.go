8f36fe561bb1a51db1312e989691a874214381a1
0xpoma <punk@DESKTOP-D6F62GU>
2023-08-27 15:55:52 +0500

    release3/0

--- new.go
+++ new.go
@@ -10,71 +10,109 @@ import (
 )
 
 func main() {
-	var choice int
+	var choice, randchoice, n, menu int
 	var countDraw, countWin, countLose int
 	fmt.Println("--Rock,Paper,Scissors--")
-	fmt.Println(">>1.Rock 2.Paper 3.Scissors")
-	// fmt.Scan(&choice)
+	// fmt.Println(">>1.Rock 2.Paper 3.Scissors")
+	fmt.Println("1.Play the game with bot")
+	fmt.Println("2.Check the result of game")
+	fmt.Scan(&menu)
+	_ = os.Remove("bar.html")
 	rand.Seed(time.Now().UnixNano())
-
-	for i := 0; i < 1000; i++ {
-		choice = rand.Intn(3) + 1
+	switch menu {
+	case 1:
+		fmt.Println(">>1.Rock 2.Paper 3.Scissors")
+		fmt.Scan(&choice)
 		intchoice := rand.Intn(3)
 		switch choice {
 		case 1: // Rock
 			if intchoice == 0 {
-				// fmt.Println("Draw")
-				countDraw++
+				fmt.Println("Draw")
 			}
 			if intchoice == 1 { //Paper
-				// fmt.Println("You lose")
-				countLose++
+				fmt.Println("You lose")
 			}
 			if intchoice == 2 {
-				// fmt.Println("You win") //Scissors
-				countWin++
+				fmt.Println("You win") //Scissors
 			}
 		case 2: //Paper
 			if intchoice == 0 {
-				// fmt.Println("You win")
-				countWin++
+				fmt.Println("You win")
 			}
 			if intchoice == 1 {
-				// fmt.Println("Draw")
-				countDraw++
+				fmt.Println("Draw")
 			}
 			if intchoice == 2 {
-				// fmt.Println("You lose")
-				countLose++
+				fmt.Println("You lose")
 			}
 		case 3: //Scissors
 			if intchoice == 0 {
-				// fmt.Println("You lose")
-				countLose++
+				fmt.Println("You lose")
 			}
 			if intchoice == 1 {
-				// fmt.Println("You win")
-				countWin++
+				fmt.Println("You win")
 			}
 			if intchoice == 2 {
-				// fmt.Println("Draw")
-				countDraw++
+				fmt.Println("Draw")
 			}
 		default:
 			fmt.Println("Something went wrong")
 		}
+
+	case 2:
+		fmt.Println("Enter the number of games you wanna to see")
+		fmt.Scan(&n)
+		defer fmt.Println("The file was created")
+		for i := 0; i < n; i++ {
+			randchoice = rand.Intn(3) + 1
+			intchoice := rand.Intn(3)
+			switch randchoice {
+			case 1: // Rock
+				if intchoice == 0 {
+					countDraw++
+				}
+				if intchoice == 1 { //Paper
+					countLose++
+				}
+				if intchoice == 2 {
+					//Scissors
+					countWin++
+				}
+			case 2: //Paper
+				if intchoice == 0 {
+					countWin++
+				}
+				if intchoice == 1 {
+					countDraw++
+				}
+				if intchoice == 2 {
+					countLose++
+				}
+			case 3: //Scissors
+				if intchoice == 0 {
+					countLose++
+				}
+				if intchoice == 1 {
+					countWin++
+				}
+				if intchoice == 2 {
+					countDraw++
+				}
+			default:
+				fmt.Println("Something went wrong")
+			}
+		}
+		bar := charts.NewBar()
+		bar.SetGlobalOptions(charts.WithTitleOpts(opts.Title{
+			Title: "Comparing ",
+		}))
+
+		bar.SetXAxis([]string{"Win", "Lose", "Draw"}).
+			AddSeries("Numbers", getDiagramm(countWin, countLose, countDraw))
+		f, _ := os.Create("bar.html")
+		bar.Render(f)
 	}
-	bar := charts.NewBar()
-	bar.SetGlobalOptions(charts.WithTitleOpts(opts.Title{
-		Title: "Comparing ",
-	}))
-
-	bar.SetXAxis([]string{"Win", "Lose", "Draw"}).
-		AddSeries("Numbers", getDiagramm(countWin, countLose, countDraw))
-	f, _ := os.Create("bar.html")
-	bar.Render(f)
 }
-
 func getDiagramm(countWin, countLose, countDraw int) []opts.BarData {
 	values := []opts.BarData{
 		{Value: countWin},
